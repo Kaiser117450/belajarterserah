b1eff5de5a6a08a03c4f4eb9d678cd54e3726bf9
StrudelPrinzessin <akmal.marzuki@protonmail.com>
2024-07-19 17:00:47 +0000

    Netlfy add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
-
     title: 'Belajarin Aja',
-	customCss: [
-        // Path to your Tailwind base styles:
-        './src/tailwind.css',
-      ],
+    customCss: [
+    // Path to your Tailwind base styles:
+    './src/tailwind.css'],
     social: {
       github: 'https://github.com/withastro/starlight'
     },
-	
     sidebar: [{
       label: 'Belajar BIG DATA',
       autogenerate: {
         directory: 'reference'
       }
     }]
-  }), tailwind()]
+  }), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
